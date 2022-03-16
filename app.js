33aadb7e029ccb28fe8f499fb9aa1c0862d27d0f
Kevin Carrington <Kevington99@gmail.com>
2022-03-15 18:08:34 -0700

    Completed first version of RPS

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-function computerPlay (choice) {
-    return Math.random ();
-}
-
-/*function computerPlay() {
-    if (Math.floor(Math.random() * 3) === 2) {
-        return('Rock');
-    } else if (Math.floor(Math.random() * 3) === 1) {
-        return('Paper');
-    } else {
-        return('Sissors')
-    }
-}
-*/
-
 function computerPlay() {
     if (Math.floor(Math.random() * 3) === 2) {
         return('rock');
@@ -27,26 +12,18 @@ function game() {
 
     let playerScore = 0;
     let computerScore = 0;
-
-    /*if ((playerScore >= 3) && (computerScore < 3)){
-        console.log('You win it all');
-    } else if ((computerScore >= 3) && (playerScore < 3)){
-        console.log('You lost it all');
-    }
-    */
-
+    let finalScore = 0;
 
     for (let i = 0; i < 5; i++) {
      
     
         function playRound(playerSelection, computerSelection) {
-
            
-        
             if (playerSelection === computerSelection){
                 console.log('It is a tie');
                 computerScore++
                 playerScore++
+                finalScore++
             } else if (
                 (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
                 (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') ||
@@ -54,31 +31,31 @@ function game() {
             ){
                  console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
                  playerScore++
+                 finalScore++
                 
             } else {
                console.log('You loose! ' + computerSelection + ' beats ' + playerSelection);
                computerScore++
+               finalScore++
             }
             console.log('Player Score: ' + playerScore);
-            console.log('Computer Score: ' + computerScore);
-              
+            console.log('Computer Score: ' + computerScore); 
           }
-
-          if ((i == 4) && (playerScore > computerScore)){
-            console.log('You win it all');
-        } else if ((i == 4) && (playerScore < computerScore)){
-            console.log('You lost it all');
-        } else if ((i == 4) && (playerScore = computerScore)){
-            console.log('It all ends in a tie');
-        }
-          
+        
           const playerSelection = prompt('Rock, Paper, Scissors?');
           const computerSelection = computerPlay();
-          console.log(playRound(playerSelection, computerSelection));
+         /* console.log(playRound(playerSelection, computerSelection)); */
           
           
 
      }
-
+     if ((finalScore == 5) && (playerScore > computerScore)){
+        console.log('You win it all!!!');
+    } else if ((finalScore == 5) && (playerScore < computerScore)){
+        console.log('You lost it all!!!');
+    } else if ((finalScore == 5) && (playerScore = computerScore)){
+        console.log('It all ends in a tie!!!');
+    }
 
 }
+game();
