c775d1256bc1e26f0a1cf9084cf7d2b2cd6414c6
Kevin Carrington <Kevington99@gmail.com>
2022-03-20 19:15:31 -0700

    Added UI. First to 5 wins

--- app.js
+++ app.js
@@ -8,54 +8,94 @@ function computerPlay() {
     }
 }
 
-function game() {
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+}
 
-    let playerScore = 0;
-    let computerScore = 0;
-    let finalScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-     
+function playRound(e) {
+    const playerChoice = e.target.textContent.toLowerCase();
+    const computerChoice = computerPlay();
+    console.log( computerScore + playerScore)
     
-        function playRound(playerSelection, computerSelection) {
-           
-            if (playerSelection === computerSelection){
-                console.log('It is a tie');
-                computerScore++
-                playerScore++
-                finalScore++
-            } else if (
-                (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') ||
-                (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') ||
-                (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock')
-            ){
-                 console.log('You win! ' + playerSelection + ' beats ' + computerSelection);
-                 playerScore++
-                 finalScore++
-                
-            } else {
-               console.log('You loose! ' + computerSelection + ' beats ' + playerSelection);
-               computerScore++
-               finalScore++
-            }
-            console.log('Player Score: ' + playerScore);
-            console.log('Computer Score: ' + computerScore); 
-          }
-        
-          const playerSelection = prompt('Rock, Paper, Scissors?');
-          const computerSelection = computerPlay();
-         /* console.log(playRound(playerSelection, computerSelection)); */
-          
-          
 
-     }
-     if ((finalScore == 5) && (playerScore > computerScore)){
-        console.log('You win it all!!!');
-    } else if ((finalScore == 5) && (playerScore < computerScore)){
-        console.log('You lost it all!!!');
-    } else if ((finalScore == 5) && (playerScore = computerScore)){
-        console.log('It all ends in a tie!!!');
+    if (playerChoice === computerChoice){
+        console.log('It is a tie');
+        resultsP.textContent = 'It is a tie';
+        score.textContent = 'Player: ' + playerScore + ' Computer: ' + computerScore;
+        finalResult.textContent = '';
+    } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'rock')
+    ){
+        playerScore++
+        console.log('You win! ' + playerChoice + ' beats ' + computerChoice);
+        resultsP.textContent = 'You win! ' + playerChoice + ' beats ' + computerChoice;
+        score.textContent = 'Player: ' + playerScore + ' Computer: ' + computerScore;
+        finalResult.textContent = '';
+    } else {
+        computerScore++
+        console.log('You loose! ' + computerChoice + ' beats ' + playerChoice);
+        resultsP.textContent = 'You loose! ' + computerChoice + ' beats ' + playerChoice;
+        score.textContent = 'Player: ' + playerScore + ' Computer: ' + computerScore;
+        finalResult.textContent = '';
     }
 
+    if (playerScore === 5 && playerScore > computerScore){
+        finalResult.textContent = 'YOU WIN IT ALL';
+        reset();
+    } else if (
+        computerScore === 5 && computerScore > playerScore){
+            finalResult.textContent = 'YOU LOOSE IT ALL';
+            reset();
+        } else if (
+            playerScore === 5 && computerScore === 5){
+            finalResult.textContent = 'IT ALL ENDS IN A TIE';
+            reset();
+        }
+        
 }
-game();
+
+//UI
+const container = document.querySelector('#container');
+
+const mainHeader = document.createElement('h2');
+mainHeader.textContent = "Rock, Paper, Scissors";
+container.appendChild(mainHeader);
+
+const smallDiv = document.createElement('div');
+container.appendChild(smallDiv);
+const btnRock = document.createElement('button');
+btnRock.textContent = "Rock";
+smallDiv.appendChild(btnRock);
+const btnPaper = document.createElement('button');
+btnPaper.textContent = "Paper";
+smallDiv.appendChild(btnPaper);
+const btnScissors = document.createElement('button');
+btnScissors.textContent = "Scissors";
+smallDiv.appendChild(btnScissors);
+
+const mediumDiv = document.createElement('div');
+container.appendChild(mediumDiv);
+const resultsP = document.createElement('p');
+mediumDiv.appendChild(resultsP);
+
+const mediumDivTwo = document.createElement('div');
+container.appendChild(mediumDivTwo);
+const score = document.createElement('p');
+mediumDiv.appendChild(score);
+
+const bottomDiv = document.createElement('div');
+container.appendChild(bottomDiv);
+const finalResult = document.createElement('p');
+bottomDiv.appendChild(finalResult);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+
+
